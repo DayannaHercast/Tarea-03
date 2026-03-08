6699e0076ebeb5e4c3aa50b8b7481fb87d9e2a37
Dayanna <dayannahernandezcr@gmail.com>
2026-03-08 16:50:42 -0600

    Implementación de mapa, ubicación, markers y distancia

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,6 +48,7 @@ dependencies {
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
+    implementation(libs.play.services.maps)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
@@ -55,4 +56,9 @@ dependencies {
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
+    implementation("com.google.maps.android:maps-compose:4.3.3")
+
+// Google Maps
+    implementation("com.google.android.gms:play-services-maps:18.2.0")
+    implementation("com.google.android.gms:play-services-location:21.0.1")
 }
--- app/src/main/java/com/example/tarea_03/Places.kt
+++
@@ -1,3 +0,0 @@
-package com.example.tarea_03
-
-//guardara los lugares del mapa y sus playlists
